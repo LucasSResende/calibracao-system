633195ed2770a1695afbb555788fbc76427b9851
LucasSResende <lucasresendesrs@gmail.com>
2026-05-21 09:48:36 -0300

    fix security

--- backend/security.py
+++ backend/security.py
@@ -3,9 +3,11 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    password = password[:72]  # ✅ evitar erro do bcrypt
+    password = password.strip()   # ✅ remove espaços
+    password = password[:72]      # ✅ evita erro do bcrypt
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
-    plain_password = plain_password[:72]  # ✅ mesmo ajuste aqui
+    plain_password = plain_password.strip()
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
